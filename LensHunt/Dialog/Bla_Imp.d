eadaa474c4eb66c27b99386adaba92b93be500f6
RoxanneSHS <cwillenbockel@aol.com>
2020-05-13 13:52:17 +0200

    v0.4

--- LensHunt/Dialog/Bla_Imp.d
+++ LensHunt/Dialog/Bla_Imp.d
@@ -1,108 +1,108 @@
 CHAIN
 IF~Global("Bla_ImpHunt","AR2600",0)~THEN Bla_Imp Meet1
-~No, no, no...this is not good.~
+@0
 DO~SetGlobal("Bla_ImpHunt","AR2600",1)~
-=~Nobody comes here for aaages and then come adventurers with noses seeking the ground.~
+=@1
 END
-++~We seek for something that you may have found, little imp.~+ Meet3
-++~That's none of your business, crazy imp.~DO~RunAwayFrom([PC],150)~EXIT
-++~You know we are seekers because you found something extraordinary just recently.~+ Meet3
+++@2+ Meet3
+++@3 DO~RunAwayFrom([PC],150)~EXIT
+++@4+ Meet3
 
 CHAIN
 IF~Global("Bla_ImpHunt","AR2600",1)~ THEN Bla_Imp Meet2
-~You again. What do you want...this is not good?~
+@5
 END
-++~We need to talk about the thing you found here. Because I need it.~+ Meet3
-++~Don't try to fool us, imp, we want the lens you have found.~+ Meet3
+++@6+ Meet3
+++@7+ Meet3
 
 CHAIN
 IF~~ THEN Bla_Imp Meet3
-~It's not yours, I know it's not. I know who lost it and it wasnt't you.~
-=~But now it's mine and mine alone.~
+@8
+=@9
 END
-++~There is surely something you may want even more than some blue lens that is useless to you.~+ Meet4
-++~I have no time to waste with an imp. They can't be reasoned with. Give me the lens you found or I take it by force.~DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
+++@10+ Meet4
+++@11 DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Meet4
-~No, no, no...I want my shiny. Is all I want...~
-=~Maybe all except for a bow, yes, I want a bow...with arrows. That's what I want.~
+@12
+=@13
 END
-++~You can't even handle a bow and arrow, imp, that is nonsense.~+ Meet5
-++~I have no time to waste with an imp. They can't be reasoned with. Give me the lens you found or I take it by force.~DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
-++~If you want a bow you get one, no problem. As long as I get the lens in return.~+ Meet5
+++@14+ Meet5
+++@11 DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
+++@15+ Meet5
 
 CHAIN
 IF~~ THEN Bla_Imp Meet5
-~Must be a short bow, that's what it's called, but a gooood one, no trash. And ten arrows with acid that hurt the nasties around here.~
+@16
 END
-IF~PartyHasItem("bow06")~THEN REPLY~This is a magical shortbow and I can give it to you.~+ Bow1
-IF~OR(4)PartyHasItem("bow06") PartyHasItem("bow14")PartyHasItem("bow15")PartyHasItem("bow18")~THEN REPLY~This is a magical shortbow but I need it still. However I know where I can get one for you as well.~DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
-IF~PartyHasItem("bow14")~THEN REPLY~Here is a bow of Tuigan origin that can be fired faster than a normal one.~+ Bow2
-IF~PartyHasItem("bow15")~THEN REPLY~Tansheron's bow is fitting and it doesn't even need arrows as it spawns phantom arrows when you fire it.~+ Bow3
-IF~PartyHasItem("bow18")~THEN REPLY~This shortbow is the rival of any of the larger bows by the magic embedded in its wood.~+ Bow4
-IF~!PartyHasItem("bow14") !PartyHasItem("bow06") !PartyHasItem("bow15") !PartyHasItem("bow18")~THEN REPLY~I have not one of those right now but I know where to get it.~DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
+IF~PartyHasItem("bow06")~THEN REPLY @17+ Bow1
+IF~OR(4)PartyHasItem("bow06") PartyHasItem("bow14")PartyHasItem("bow15")PartyHasItem("bow18")~THEN REPLY @18 DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
+IF~PartyHasItem("bow14")~THEN REPLY @19+ Bow2
+IF~PartyHasItem("bow15")~THEN REPLY @20+ Bow3
+IF~PartyHasItem("bow18")~THEN REPLY @21+ Bow4
+IF~!PartyHasItem("bow14") !PartyHasItem("bow06") !PartyHasItem("bow15") !PartyHasItem("bow18")~THEN REPLY @22 DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
 
 CHAIN
 IF~~ THEN Bla_Imp Meet6
-~Excellent...and not forget those arrows!~EXIT
+@23 EXIT
 
 CHAIN
 IF~Global("Bla_ImpHunt","AR2600",2)~ THEN Bla_Imp Bow0
-~Sooo...you done shopping a bit?~
+@24
 END
-IF~PartyHasItem("bow06")~THEN REPLY~This is a magical shortbow and I can give it to you.~+ Bow1
-IF~PartyHasItem("bow14")~THEN REPLY~Here is a bow of Tuigan origin that can be fired faster than a normal one.~+ Bow2
-IF~PartyHasItem("bow15")~THEN REPLY~Tansheron's bow is fitting and it doesn't even need arrows as it spawns phantom arrows when you fire it.~+ Bow3
-IF~PartyHasItem("bow18")~THEN REPLY~This shortbow is the rival of any of the larger bows by the magic embedded in its wood.~+ Bow4
-IF~!PartyHasItem("bow14") !PartyHasItem("bow06") !PartyHasItem("bow15") !PartyHasItem("bow18")~THEN REPLY~I have not one of those right now but I know where to get it.~DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
-++~I have no time to waste with an imp. They can't be reasoned with. Give me the lens you found or I take it by force.~DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
+IF~PartyHasItem("bow06")~THEN REPLY @17+ Bow1
+IF~PartyHasItem("bow14")~THEN REPLY @19+ Bow2
+IF~PartyHasItem("bow15")~THEN REPLY @20+ Bow3
+IF~PartyHasItem("bow18")~THEN REPLY @21+ Bow4
+IF~!PartyHasItem("bow14") !PartyHasItem("bow06") !PartyHasItem("bow15") !PartyHasItem("bow18")~THEN REPLY  @22 DO~SetGlobal("Bla_ImpHunt","AR2600",2)~+ Meet6
+++@11 DO~SetGlobal("Bla_ImpHunt","AR2600",3)~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow1
-~Excellent...and what with arrows?~
+@25
 END
-IF~NumItemsPartyGT("arow04",9)~THEN REPLY~Here they are.~DO~TakePartyItemNum("arow04",10)~ + Bow11
-IF~NumItemsPartyLT("arow04",10)~THEN REPLY~I'm afraid I have none.~ + Bow12
+IF~NumItemsPartyGT("arow04",9)~THEN REPLY @26 DO~TakePartyItemNum("arow04",10)~ + Bow11
+IF~NumItemsPartyLT("arow04",10)~THEN REPLY @27 + Bow12
 
 CHAIN
 IF~~ THEN Bla_Imp Bow2
-~Excellent...and what with arrows?~
+@25
 END
-IF~NumItemsPartyGT("arow04",9)~THEN REPLY~Here they are.~DO~TakePartyItemNum("arow04",10)~ + Bow21
-IF~NumItemsPartyLT("arow04",10)~THEN REPLY~I'm afraid I have none.~ + Bow22
+IF~NumItemsPartyGT("arow04",9)~THEN REPLY @26 DO~TakePartyItemNum("arow04",10)~ + Bow21
+IF~NumItemsPartyLT("arow04",10)~THEN REPLY @27 + Bow22
 
 CHAIN
 IF~~ THEN Bla_Imp Bow3
-~Excellent...Nooo need to carry arrows. I take it.~DO~TakePartyItem("bow15")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@28 DO~TakePartyItem("bow15")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow4
-~Excellent...and what with arrows?~
+@25
 END
-IF~NumItemsPartyGT("arow04",9)~THEN REPLY~Here they are.~DO~TakePartyItemNum("arow04",10)~ + Bow41
-IF~NumItemsPartyLT("arow04",10)~THEN REPLY~I'm afraid I have none.~ + Bow42
+IF~NumItemsPartyGT("arow04",9)~THEN REPLY @26 DO~TakePartyItemNum("arow04",10)~ + Bow41
+IF~NumItemsPartyLT("arow04",10)~THEN REPLY @27 + Bow42
 
 CHAIN
 IF~~ THEN Bla_Imp Bow11
-~Excellent...I take it.~DO~TakePartyItem("bow06")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@29 DO~TakePartyItem("bow06")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow12
-~Nevermind, enough wood and acid around to make my own. I take the bow.~DO~TakePartyItem("bow06")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@30 DO~TakePartyItem("bow06")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow21
-~Excellent...I take it.~DO~TakePartyItem("bow14")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@29 DO~TakePartyItem("bow14")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow22
-~Nevermind, enough wood and acid around to make my own. I take the bow.~DO~TakePartyItem("bow14")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@30 DO~TakePartyItem("bow14")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow41
-~Excellent...I take it.~DO~TakePartyItem("bow18")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@29 DO~TakePartyItem("bow18")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
 
 CHAIN
 IF~~ THEN Bla_Imp Bow42
-~Nevermind, enough wood and acid around to make my own. I take the bow.~DO~TakePartyItem("bow18")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
+@30 DO~TakePartyItem("bow18")GiveItem("c6Lens3",Player1) EscapeAreaDestroy(20) ~EXIT
--- LensHunt/Dialog/Bla_LensMain.d
+++ LensHunt/Dialog/Bla_LensMain.d
@@ -5,58 +5,58 @@ BEGIN Bla_Ogr3
 BEGIN Bla_Ogr4
 
 CHAIN
-IF WEIGHT #-13 ~ Global("ElvenCityTree","GLOBAL",0)Global("Bla_Lenshunt","Global",1)~ THEN C6Elhan2 FalseLense
-~You return. All has been relatively quiet here. I believe the drow were mostly satisfied with the destruction of our temple. They have made no further serious advancement. Yet.~ [ELHAN110]
+IF WEIGHT #-13 ~Global("ElvenCityTree","GLOBAL",0)Global("Bla_Lenshunt","Global",1)~ THEN C6Elhan2 FalseLense
+@0
 END
 IF ~PartyHasItem("Bla_Lat1")~ THEN GOTO 66
 IF ~!PartyHasItem("c6lantho") !PartyHasItem("Bla_Lat1") ~ THEN GOTO 65
 
 INTERJECT C6Elhan2 78 BLA_LensElhan
-==C6Warsa1 IF~Global("Bla_Lenshunt","Global",1)~THEN~Wait!~
-==C6Warsa2 IF~Global("Bla_Lenshunt","Global",1)~THEN~Wait!~
-==C6Elhan2 IF~Global("Bla_Lenshunt","Global",1)~THEN~What is wrong, my sages, we cannot delay our journey to Suldanessalar much longer?~
-==C6Warsa1 IF~Global("Bla_Lenshunt","Global",1)~THEN~The Lanthorn is not in focus, the picture is unclear.~
-==C6Warsa2 IF~Global("Bla_Lenshunt","Global",1)~THEN~The lenses, it's the lenses. Three of them are missing.~
-==C6Elhan2 IF~Global("Bla_Lenshunt","Global",1)~THEN~<CHARNAME>? You owe me some explanation. Now.~
+==C6Warsa1 IF~Global("Bla_Lenshunt","Global",1)~THEN @1
+==C6Warsa2 IF~Global("Bla_Lenshunt","Global",1)~THEN @1
+==C6Elhan2 IF~Global("Bla_Lenshunt","Global",1)~THEN @2
+==C6Warsa1 IF~Global("Bla_Lenshunt","Global",1)~THEN @3
+==C6Warsa2 IF~Global("Bla_Lenshunt","Global",1)~THEN @4
+==C6Elhan2 IF~Global("Bla_Lenshunt","Global",1)~THEN @5
 END
-++~This is how we found the Lanthorn in Bodhi's lair. We have not done any damage to the thing.~EXTERN C6Warsa1 Misslens1
-++~I already noticed the missing lenses but I had no way of knowing on such an old artefact that this was a recent demage.~EXTERN C6Warsa1 Misslens1
-++~Be careful, Elhan, I have nothing to do with the state your lamp is in.~EXTERN C6Warsa1 Misslens1
+++@6 EXTERN C6Warsa1 Misslens1
+++@7 EXTERN C6Warsa1 Misslens1
+++@8 EXTERN C6Warsa1 Misslens1
 
 CHAIN
 IF~~THEN C6Warsa1 Misslens1
-~True!~
+@9
 DO~SetGlobal("Bla_Lenshunt","Global",2)~
-==C6Warsa2 ~True, <PRO_HESHE> had no way of knowing that those pieces are essential for the Lanthorn.~
-==C6Warsa1 ~The hunt?~
-==C6Warsa2 ~It must have happened during the hunt.~
+==C6Warsa2 @10
+==C6Warsa1 @11
+==C6Warsa2 @12
 END
-++~True, yes, true!~EXTERN C6Elhan2 Misslens2
-++~The hunt?~EXTERN C6Elhan2 Misslens2
-++~Do these stooges ever say something meaningful?~EXTERN C6Elhan2 Misslens2
+++@13 EXTERN C6Elhan2 Misslens2
+++@11 EXTERN C6Elhan2 Misslens2
+++@14 EXTERN C6Elhan2 Misslens2
 
 CHAIN
 IF~~THEN C6Elhan2 Misslens2
-~The hunt for the neck-biter. Bodhi. My soldiers noticed her on what must have been the night when she emerged from the Underdark. They didn't know which creature they were hunting but now in hindsight it becomes clear.~
-=~They chased what they swore was a vampire and they chased her through the Tethir Forest first to the north and then west to the Small Teeth Pass where she left our territory so they couldn't follow.~
-==C6Warsa2 ~The lenses must have fallen from the Lanthorn during the chase.~
-==C6Warsa1 ~Nothing can break the magical lenses of the Lanthorn. They must be intact and can be put back into their places.~
+@15
+=@16
+==C6Warsa2 @17
+==C6Warsa1 @18
 END
-++~Just to put that straight, you ask me to find three lenses lost in an area as large as the Tethir Forest, is it that?~ + Misslens3
-++~You have a whole army to search every tree in that damned forest, good luck.~  + Misslens3
+++@19 + Misslens3
+++@20 + Misslens3
 
 CHAIN
 IF~~THEN C6Elhan2 Misslens3
-~(Sigh) I cannot spare any of my elves that hold off the drow, <CHARNAME>. Just remember that you need that repaired Lanthorn just as badly as I need it. Like it or not, our only option to success is that you find those three lenses.~
-=~We can only hope that they were really lost in that hunt and are still somewhere in the surrounding areas and were not removed by Bodhi and hidden who knows where...~
-==C6Warsa1 ~Try the North Forest or the Small Teeth Pass.~DO~ReallyForceSpell(Myself,DRYAD_TELEPORT)~
-==C6Warsa2 ~Search the Tehir Forest itself. Be quick.~DO~ReallyForceSpell(Myself,DRYAD_TELEPORT)~
-==C6Elhan2~All speed to you, <CHARNAME>.~EXIT
+@21
+=@22
+==C6Warsa1 @23 DO ~ReallyForceSpell(Myself,DRYAD_TELEPORT)~
+==C6Warsa2 @24 DO ~ReallyForceSpell(Myself,DRYAD_TELEPORT)~
+==C6Elhan2 @25 EXIT
 
 CHAIN
 IF WEIGHT #-14 ~ Global("Bla_Lenshunt","Global",4)~ THEN C6Elhan2 RightLense
-~My sages already informed me that the Lanthorn is restored. Good work, <CHARNAME>.~
-DO~SetGlobal("Bla_Lenshunt","Global",5)~
-=~We must leave immediadely, but most of my elves must remain to hold the drow. We must not be struggling on two fronts when we face what is in the city.~
+@26
+DO ~SetGlobal("Bla_Lenshunt","Global",5)~
+=@27
 END
 IF ~~ THEN GOTO 78
